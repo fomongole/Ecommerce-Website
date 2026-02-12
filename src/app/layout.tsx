1925ee79583c8de5b5656fb1adff95e962733884
Fred Omongole <fomongole091600@gmail.com>
2026-02-12 09:37:36 +0300

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -35,7 +35,6 @@ export default function RootLayout({
           enableSystem={false}
           disableTransitionOnChange
         >
-          {/* 2. Wrap everything inside AuthProvider */}
           <AuthProvider>
             <Navbar />
             {children}
